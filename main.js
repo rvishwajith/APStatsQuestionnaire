cc6523d7acb095913f300f7fdea1d6cc166b32ec
Rohith Vishwajith <rohithv2312@gmail.com>
2021-03-05 20:26:50 -0800

    c

--- main.js
+++ main.js
@@ -10,11 +10,10 @@ var set = false;
 class Page {
 
     constructor() {
-        
         this.title = "";
         this.div = "";
         this.innerDiv = "";
-        this.children = "";
+        this.containsAnswers = false;
     }
 }
 
@@ -71,7 +70,6 @@ $(document).ready(function() {
 
             updateProgressBar();
         }
-
         
     });
 
@@ -80,18 +78,97 @@ $(document).ready(function() {
         
         if(currentPage < pages.length && set) {
 
-            console.log($('StudentIDInput').val());
-
             let newRight = (currentPage + 1) * -100 + "%";
             console.log(newRight);
-
             $(".Page").animate({left: newRight});
             currentPage++;
+
+            var studentID = document.getElementById("StudentIDInput").value;        
+            var studentIDAnswer = document.getElementById("StudentIDAnswer");
+            studentIDAnswer.innerText = studentID;
 
             updateProgressBar();
         }
     });
 
+    $(document).on("click",".YesNoButton", function (event) {
+        
+        let parent = $(event.target).parent();
+        let answer = $(event.target).get(0).innerText;
+        
+        let div = parent.get(0).getElementsByClassName("YesNoButtonActive")[0];
+        let div2 = parent.get(0).getElementsByClassName("YesNoActive")[0];
+        let form = parent.get(0).getElementsByClassName("YesNoButton");
+        let yesNo = div2.innerText;
+        
+
+        if(answer != yesNo) {
+            
+            if(answer == "Yes") {
+                // shift to the left
+                $(div).animate({left: "2px"}, 200);
+                form[0].className += " YesNoActive";
+                form[1].className = form[1].className.replace(" YesNoActive", "");
+            }
+            else if(answer == "No") {
+                // shift to the right
+                $(div).animate({left: "54px"}, 200);
+                form[1].className += " YesNoActive";
+                form[0].className = form[1].className.replace(" YesNoActive", "");
+            }
+        }
+    });
+
+    $(document).on("click",".A1234", function (event) {
+
+        let parent = $(event.target).parent();
+        let answer = $(event.target).get(0).innerText;
+
+        let div = parent.get(0).getElementsByClassName("A1234ButtonActive")[0];
+        let div2 = parent.get(0).getElementsByClassName("A1234Active")[0];
+        let form = parent.get(0).getElementsByClassName("A1234Button");
+        let ans = div2.innerText
+
+        if(answer != ans) {
+
+            var num;
+
+            for(var i = 0; i < form.length; i++) {
+                if(answer == form[i].innerText) {
+                    num = i;
+                }
+            }
+
+            for(var i = 0; i < form.length; i++) {
+                form[i].className = form[1].className.replace(" A1234Active", "");
+            }
+            form[num].className += " A1234Active";
+            let amnt = 2 + 42 * num + "px";
+            $(div).animate({left: amnt}, 200);
+        }
+
+    });
+
+    $(document).on("click",".MultipleChoice", function (event) {
+
+        let parent = $(event.target).parent();
+        let answer = $(event.target).get(0).innerText;
+
+        let div2 = parent.get(0).getElementsByClassName("ChoiceActive")[0];
+        let form = parent.get(0).getElementsByClassName("Choice");
+        let ans = div2.innerText
+
+        for(var i = 0; i < form.length; i++) {
+            if(answer == form[i].innerText) {
+                num = i;
+            }
+        }
+
+        for(var i = 0; i < form.length; i++) {
+            form[i].className = form[1].className.replace(" ChoiceActive", "");
+        }
+        form[num].className += " ChoiceActive";
+    });
 });
 
 function updateProgressBar() {
@@ -104,7 +181,7 @@ async function setWebpage() {
     
     let markupLines = markup.split("\n");
     var currentPage = -1;
-    
+    var numButtons = -1;
 
     for(var i = 0; i < markupLines.length; i++) {
         
@@ -127,7 +204,9 @@ async function setWebpage() {
                 div3.innerText = "Continue";
                 div3.className += "NavForward";
 
-                div.appendChild(div2);
+                if(currentPage != 0) {
+                    div.appendChild(div2);
+                }
                 div.appendChild(div3);
 
                 pages[currentPage].innerDiv.appendChild(br);
@@ -209,6 +288,30 @@ async function setWebpage() {
             div.innerText = document.getElementById("StudentIDInput").value + "1234567";
 
             pages[currentPage].innerDiv.appendChild(div);
+        }
+        else if(line.startsWith("|Answer|")) {
+
+            first = false;
+            let text = line.replace("|Answer|", "");
+
+            if(pages[currentPage].containsAnswers == false) {
+                let div = document.createElement("div");
+                div.className = "MultipleChoice";
+                pages[currentPage].innerDiv.appendChild(div);
+                first = true;
+                pages[currentPage].containsAnswers = true;
+            }
+
+            let holder = pages[currentPage].innerDiv.getElementsByClassName("MultipleChoice")[0];
+            let div = document.createElement("div");
+            div.className = "Choice";
+            div.innerText = text;
+
+            if(first == true) {
+                div.className += " ChoiceActive";
+            }
+
+            holder.appendChild(div);
         }
     }
 
